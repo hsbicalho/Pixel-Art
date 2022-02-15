bb9dec9bc799dba76ff116dc5760ceb97722827d
Heitor <hsbicalho@gmail.com>
2022-02-15 16:57:33 -0300

    requisitos 7, 8 e 9

--- script.js
+++ script.js
@@ -1,19 +1,52 @@
-function criaBoard(columns, lines) {
-  const board = document.querySelector('#pixel-board');
-  for (let i = 0; i < columns; i += 1) {
-    const pixelColumn = document.createElement('tr');
-    board.appendChild(pixelColumn);
-    for (let j = 0; j < lines; j += 1) {
-      const pixelLine = document.createElement('td');
-      pixelLine.className = 'pixel';
-      pixelColumn.appendChild(pixelLine);
-    }
+function criaPixel(quant) {
+  const pixelBoard = document.querySelector('#pixel-board');
+  for (let i = 0; i < quant; i += 1) {
+    const pixel = document.createElement('div');
+
+    pixel.className = 'pixel';
+    pixelBoard.appendChild(pixel);
   }
 }
-criaBoard(5, 5);
-
+criaPixel(25);
 function inicialSelected() {
   const color1 = document.querySelector('#colorBlack');
   color1.classList.add('selected');
 }
 inicialSelected();
+function setSelected(evento) {
+  document.querySelector('.selected').classList.remove('selected');
+  evento.target.classList.add('selected');
+}
+
+document.querySelector('#colorBlack').addEventListener('click', setSelected);
+document.querySelector('#colorRed').addEventListener('click', setSelected);
+document.querySelector('#colorBlue').addEventListener('click', setSelected);
+document.querySelector('#colorGreen').addEventListener('click', setSelected);
+
+function selectedColor() {
+  const colorSelected = document.querySelector('.selected');
+  return colorSelected.classList[1];
+}
+
+function paint() {
+  const arrayPixels = document.querySelectorAll('.pixel');
+  for (let i = 0; i < arrayPixels.length; i += 1) {
+    const pixel = arrayPixels[i];
+    pixel.addEventListener('click', () => {
+      pixel.style.backgroundColor = `${selectedColor()}`;
+    });
+  }
+}
+paint();
+
+function clearPixels() {
+  const button = document.querySelector('#clear-board');
+  const arrayPixels = document.querySelectorAll('.pixel');
+  button.addEventListener('click', () => {
+    for (let i = 0; i < arrayPixels.length; i += 1) {
+      const pixel = arrayPixels[i];
+      pixel.style.backgroundColor = 'white';
+    }
+  });
+}
+clearPixels();
