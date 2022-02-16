6159108fba526cdd3b1f7869e9e6d98267142b14
Heitor <hsbicalho@gmail.com>
2022-02-15 21:02:00 -0300

    conclusao requisitos 10 11 e 12

--- script.js
+++ script.js
@@ -10,17 +10,6 @@ function criaPixel(quant) {
   }
 }
 criaPixel(25);
-query('#generate-board').addEventListener('click', () => {
-  query('#pixel-board').innerText = '';
-  const boardSize = query('#board-size').value;
-  const quant = boardSize * boardSize;
-  pixelBoard.style.grid = `auto-flow / repeat(${boardSize}, auto)`;
-  for (let i = 0; i < quant; i += 1) {
-    const pixel = document.createElement('div');
-    pixel.className = 'pixel';
-    pixelBoard.appendChild(pixel);
-  }
-});
 
 function inicialSelected() {
   const color1 = query('#colorBlack');
@@ -31,6 +20,12 @@ function setSelected(evento) {
   query('.selected').classList.remove('selected');
   evento.target.classList.add('selected');
 }
+const color2 = Math.floor(Math.random() * 16777215).toString(16);
+query('#colorRed').style.backgroundColor = `#${color2}`;
+const color3 = Math.floor(Math.random() * 16777215).toString(16);
+query('#colorBlue').style.backgroundColor = `#${color3}`;
+const color4 = Math.floor(Math.random() * 16777215).toString(16);
+query('#colorGreen').style.backgroundColor = `#${color4}`;
 
 query('#colorBlack').addEventListener('click', setSelected);
 query('#colorRed').addEventListener('click', setSelected);
@@ -38,8 +33,10 @@ query('#colorBlue').addEventListener('click', setSelected);
 query('#colorGreen').addEventListener('click', setSelected);
 
 function selectedColor() {
-  const colorSelected = query('.selected');
-  return colorSelected.classList[1];
+  const colorSelected = query('.selected'); // ajuda do joão pster aqui!! para conseguir acessar a cor do background https://github.com/tryber/sd-020-b-project-pixels-art/blob/joao-pster-project-pixels-art/script.js
+  const cssObject = window.getComputedStyle(colorSelected, null);
+  const backgroundgColor = cssObject.getPropertyValue('background-color');
+  return backgroundgColor;
 }
 
 function paint() {
@@ -64,3 +61,26 @@ function clearPixels() {
   });
 }
 clearPixels();
+
+query('#generate-board').addEventListener('click', () => {
+  query('#pixel-board').innerText = '';
+  const boardSize = query('#board-size').value;
+  if (boardSize === '') return alert('Board inválido!');
+  if (boardSize < 5) {
+    pixelBoard.style.grid = 'auto-flow / repeat(5, auto)';
+    return criaPixel(25);
+  }
+  if (boardSize > 50) {
+    pixelBoard.style.grid = 'auto-flow / repeat(50, auto)';
+    return criaPixel(2500);
+  }
+  const quant = boardSize * boardSize;
+  pixelBoard.style.grid = `auto-flow / repeat(${boardSize}, auto)`;
+  for (let i = 0; i < quant; i += 1) {
+    const pixel = document.createElement('div');
+    pixel.className = 'pixel';
+    pixelBoard.appendChild(pixel);
+  }
+  paint();
+  clearPixels();
+});
